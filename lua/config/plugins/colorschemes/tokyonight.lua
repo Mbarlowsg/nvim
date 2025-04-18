22bfc84585a4675145a4e8e01c94af17bc665472
Mbarlowsg <mbarlowsg@yahoo.com>
2025-04-18 13:21:40 +0800

    small changes to tokyonight

--- lua/config/plugins/colorschemes/tokyonight.lua
+++ lua/config/plugins/colorschemes/tokyonight.lua
@@ -3,8 +3,22 @@ return {
     enabled = true,
     lazy = false,
     priority = 1000,
-    opts = {},
-    config = function()
-        vim.cmd("colorscheme tokyonight-moon") -- add additional options: 'moon', 'storm', 'night', 'day'
+    opts = {
+        -- Change the style 
+        style = "night", -- options: 'moon', 'storm', 'night', 'day'
+        transparent = false, -- enable to disable setting the background
+        -- Set styles for different groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        styles = {
+            comments = { italic = false}, -- set to true by default
+            keywords = { italic = false},
+            functions = {},
+            variables = {},
+        },
+    },
+    config = function(_, opts)
+        require("tokyonight").setup(opts)
+        vim.cmd("colorscheme tokyonight") 
     end,
 }
+
